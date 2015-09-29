0fe042da5aade5e2f477e605d73cc95312b66a78
EricLee <okerivy@gmail.com>
2015-09-30 01:15:14 +0800

    添加子控制器

--- Coursera/Coursera/Classes/Module/Main/MainViewController.swift
+++ Coursera/Coursera/Classes/Module/Main/MainViewController.swift
@@ -12,24 +12,44 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addChildViewControllers()
         // Do any additional setup after loading the view.
     }
 
+    /// 添加所有的子控制器
+    private func addChildViewControllers() {
+        
+        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+
+        
+    }
+    
+  
+    ///  添加子控制器
+    ///
+    ///  - parameter vc:        子控制器
+    ///  - parameter title:     标题
+    ///  - parameter imageName: 图片名称
+    private func addChildViewController(vc: UIViewController, title: String, imageName: String) {
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        vc.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        let nav = UINavigationController(rootViewController: vc)
+        addChildViewController(nav)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
